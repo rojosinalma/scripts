7a2afa777b8616035f4820adf0b0273c1315aa54
Feña Agar <fernando.agar@gmail.com>
2018-10-21 14:37:43 +0200

    Using whenever and added crons for jacket and plex

--- feralhosting/croncheck.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-[[ $(pgrep -fu $(whoami) 'JackettConsole.exe') ]] || ./start_jackett.sh
--- feralhosting/restart_jackett.sh
+++ feralhosting/restart_jackett.sh
@@ -1,1 +1,5 @@
+kill "$(pgrep -fu "$(whoami)" "JackettConsole.exe")"
+
+sleep 2
+
 screen -dmS jackett mono --debug ~/Jackett/JackettConsole.exe && echo -e "\nhttp://$(hostname -f):$(sed -rn 's|(.*)"Port": (.*),|\2|p' ~/.config/Jackett/ServerConfig.json)"
