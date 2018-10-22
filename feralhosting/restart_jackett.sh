8d348c46db1cba1c13c6d2ce8ac27fb21657e56b
Feña Agar <ferliagno@gmail.com>
2018-10-22 19:20:04 +0200

    Update restart_jackett.sh

--- feralhosting/restart_jackett.sh
+++ feralhosting/restart_jackett.sh
@@ -2,4 +2,4 @@ kill "$(pgrep -fu "$(whoami)" "JackettConsole.exe")"
 
 sleep 2
 
-screen -dmS jackett && screen -S jackett -p 0 -X stuff "export TMPDIR=$HOME/tmp; ~/bin/mono --debug ~/.jackett/JackettConsole.exe^M"
+screen -dmS jackett && screen -S jackett -p 0 -X stuff "export TMPDIR=$HOME/tmp; ~/bin/mono --debug ~/Jackett/JackettConsole.exe^M"
