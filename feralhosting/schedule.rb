7fdec2edc5b9f3075797484b4a7cd68d4cd3bd1d
Feña Agar <ferliagno@gmail.com>
2018-10-22 19:22:27 +0200

    Update schedule.rb

--- feralhosting/schedule.rb
+++ feralhosting/schedule.rb
@@ -3,9 +3,9 @@ set :chronic_options, hours24: true
 set :output, "$HOME/cron_log.log"
 
 every 1.day, at: '22:00' do
-  command ". $HOME/.profile; ruby $HOME/scripts/feralhosting/plex_update.rb"
+  command ". $HOME/.profile; ruby $HOME/scripts/feralhosting/plex_update.rb >> ~/scripts/feralhosting/plex_update.log"
 end
 
 every 1.day, at: '3:00' do
-  command ". $HOME/.profile; $HOME/scripts/feralhosting/restart_jackett.sh >> ~/scripts/feralhosting/plex_update.log"
+  command ". $HOME/.profile; $HOME/scripts/feralhosting/start_jackett.sh"
 end
