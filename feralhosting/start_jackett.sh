ed797cea9c69df345ed2363ed2ac32b2c07badcb
Feña Agar <ferliagno@gmail.com>
2018-10-22 19:21:47 +0200

    Update and rename restart_jackett.sh to start_jackett.sh

--- feralhosting/start_jackett.sh
+++ feralhosting/start_jackett.sh
@@ -1,5 +1,1 @@
-kill "$(pgrep -fu "$(whoami)" "JackettConsole.exe")"
-
-sleep 2
-
 screen -dmS jackett && screen -S jackett -p 0 -X stuff "export TMPDIR=$HOME/tmp; ~/bin/mono --debug ~/Jackett/JackettConsole.exe^M"
