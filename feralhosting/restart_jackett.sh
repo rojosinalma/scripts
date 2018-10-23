0325262f52f831c3be9d44cd706855e42746733f
Feña Agar <fernando.agar@gmail.com>
2018-10-23 11:32:27 +0200

    Adjusted output

--- feralhosting/restart_jackett.sh
+++ feralhosting/restart_jackett.sh
@@ -1,3 +1,6 @@
+echo "INFO -- : Stopping previous Jackett screen"
 screen -X -S jackett kill
 sleep 2
+
+echo "INFO -- : Starting Jackett again"
 screen -dmS jackett && screen -S jackett -p 0 -X stuff "export TMPDIR=$HOME/tmp; ~/bin/mono --debug ~/Jackett/JackettConsole.exe^M"
--- feralhosting/schedule.rb
+++ feralhosting/schedule.rb
@@ -3,7 +3,7 @@ set :chronic_options, hours24: true
 set :output, "$HOME/cron_log.log"
 
 every 1.day, at: '22:00' do
-  command ". $HOME/.profile; ruby $HOME/scripts/feralhosting/plex_update.rb >> ~/scripts/feralhosting/plex_update.log"
+  command ". $HOME/.profile; ruby $HOME/scripts/feralhosting/plex_update.rb"
 end
 
 every 1.day, at: '3:00' do
