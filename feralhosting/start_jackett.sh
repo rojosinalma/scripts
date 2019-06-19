cacbb70a444645b9e524f56b8e1173fc84e878c8
Rojo <rojo@rojo.dev>
2019-06-19 11:49:23 +0200

    Added log to jackett screen

--- feralhosting/start_jackett.sh
+++ feralhosting/start_jackett.sh
@@ -2,4 +2,4 @@
 
 echo "INFO -- : Starting Jackett"
 screen -dmS jackett \
-/bin/bash -c 'export TMPDIR=$HOME/tmp; ~/Jackett/jackett'
+/bin/bash -c 'export TMPDIR=$HOME/tmp; ~/Jackett/jackett | tee $HOME/Jackett/screen.log'
