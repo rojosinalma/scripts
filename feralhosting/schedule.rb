327ec98605f11f2435c447b2fd869823d44ed4ea
Rojo <rojo@rojo.dev>
2019-07-26 14:21:41 +0200

    Small updates

--- feralhosting/schedule.rb
+++ feralhosting/schedule.rb
@@ -1,6 +1,6 @@
 set :job_template, nil
 set :chronic_options, hours24: true
-set :output, "$HOME/cron_log.log"
+set :output, "$HOME/cron.log"
 
 every 1.day, at: '22:00' do
   command ". $HOME/.profile; ruby $HOME/scripts/feralhosting/plex_update.rb"
