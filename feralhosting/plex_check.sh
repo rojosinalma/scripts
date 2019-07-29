edd9049bd79834d6e291e3d9de61c7b5dea7d81a
Rojo <rojo@rojo.dev>
2019-07-29 14:54:31 +0200

    Logs on plex check

--- feralhosting/plex_check.sh
+++ feralhosting/plex_check.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "[$(date)]  INFO -- : Checking if Plex is running"
 plex_count=$(ps ux | fgrep "/Plex Media Server" | wc -l)
 
 if [ $plex_count -lt 3 ]; then
+  echo "[$(date)]  INFO -- : Plex not running! ($(plex_count)) Restarting."
   . kill_plex.sh
 fi
